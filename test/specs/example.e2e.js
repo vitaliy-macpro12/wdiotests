b93f09b19697485daec050186cfea3c34fbfb2e9
vitaliy-macpro12 <59976484+vitaliy-macpro12@users.noreply.github.com>
2021-03-22 17:46:19 -0700

    New autoTest

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,14 +1,32 @@
 import LoginPage from  '../pageobjects/login.page';
-import SecurePage from '../pageobjects/secure.page';
+import ProfilePage from '../pageobjects/profile.page';
 
-describe('My Login application', () => {
-    it('should login with valid credentials', () => {
+describe('Auth', () => {
+    beforeEach(() => {
         LoginPage.open();
+    });
 
-        LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        expect(SecurePage.flashAlert).toBeExisting();
-        expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+    afterEach(() => {
+        browser.execute('window.localStorage.clear()');
+    })
+
+    it('user logs in with valid data', () => {
+        LoginPage.setLogin('wavem41476@naymio.com');
+        LoginPage.setPassword('Toyota');
+        LoginPage.clickSubmitButton();
+        ProfilePage.isOpen();
+
+    });
+
+    it('submit button is disabled if login and password are absent', () => {
+        LoginPage.submitButtonIsDisabled();
+    });
+
+    it('fails if invalid data provided', () => {
+        LoginPage.setLogin('example@example.com');
+        LoginPage.setPassword('123456');
+        LoginPage.clickSubmitButton();
+        LoginPage.errorToastAppeared();
     });
 });
 
